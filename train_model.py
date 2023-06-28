36e7485cfdb862b80dfeebae455c1e949e6a2547
Carlos <proyectos@daten.com.co>
2023-06-28 21:52:28 +0000

    Debugging / Endpoint

---
+++ train_model.py
@@ -0,0 +1,176 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+import argparse
+import json
+import logging
+import os
+import sys
+
+import smdebug.pytorch as smd
+
+from smdebug import modes
+from smdebug.profiler.utils import str2bool
+from smdebug.pytorch import get_hook
+
+#TODO: Import dependencies for Debugging andd Profiling
+
+def test(model, loss_criterion, use_cuda, test_loader, hook):
+    
+    model.eval()
+    hook.set_mode(smd.modes.EVAL)
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            if use_cuda:
+                data, target = data.cuda(), target.cuda()
+            output = model(data)
+            test_loss += loss_criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+    
+
+def train(model, loss_criterion, use_cuda, train_loader, optimizer, epoch, hook):
+    
+    from PIL import ImageFile
+    import time
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+   
+    model.train()
+    hook.set_mode(smd.modes.TRAIN)
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if use_cuda:
+            data, target = data.cuda(), target.cuda()
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(data),
+                        len(train_loader.dataset),
+                        100.0 * batch_idx / len(train_loader),
+                        loss.item(),
+                    )
+                )  
+                
+def net():
+    
+    model = models.resnet50(pretrained=True)
+    
+    for param in model.parameters():
+        param.requires_grad = False 
+        
+    num_features=model.fc.in_features
+    
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 133))
+
+    return model
+
+
+def main(args):
+    
+    model=net()
+    
+    if args.gpu:
+        model.cuda()
+    
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
+    
+    training_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224), 
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    testing_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    trainset= torchvision.datasets.ImageFolder(root=args.train, transform=training_transform)
+    train_loader = torch.utils.data.DataLoader(trainset, args.batch_size, shuffle=True)
+    
+    testset= torchvision.datasets.ImageFolder(root=args.test, transform=testing_transform)
+    test_loader = torch.utils.data.DataLoader(testset, args.batch_size, shuffle=True)
+    
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
+    hook.register_loss(loss_criterion)
+    
+    for epoch in range(1, args.epochs + 1):
+        train(model, loss_criterion, args.gpu, train_loader, optimizer, epoch, hook)
+        test(model, loss_criterion, args.gpu, test_loader, hook)
+    
+    print("Saving the model")
+    path = os.path.join(args.model_dir, "model.pth")
+    torch.save(model.cpu().state_dict(), path)
+    
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=2,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=0.01, metavar="LR", help="learning rate (default: 0.01)"
+    )
+    parser.add_argument(
+        "--momentum", type=float, default=0.5, metavar="M", help="SGD momentum (default: 0.5)"
+    )
+    
+    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--test', type=str, default=os.environ['SM_CHANNEL_TEST'])
+    parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
+    parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
+    parser.add_argument("--gpu", type=str2bool, default=True)
+    
+    args=parser.parse_args()
+    
+    main(args)
